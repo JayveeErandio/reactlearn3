6b71fbda4e7f52ffee4bb0f3362afc03be22d7d4
JayveeErandio <jayveeerandio@gmail.com>
2026-04-05 13:22:37 +0800

    Dark Mode Success

--- src/components/DarkModeButton.jsx
+++ src/components/DarkModeButton.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
 
 export default function DarkModeButton({ className }) {
-  const temp = document.querySelector("body");
-  let [mode, setMode] = useState("light");
-  const oppose = () => {
-    if (mode == "light") return "dark";
-    else return "light";
+  const [theme, setTheme] = useState("light");
+
+  const oppose = function () {
+    return theme == "light" ? "dark" : "light";
   };
-  temp.setAttribute("data-theme", mode);
+
+  useEffect(() => {
+    document.querySelector("body").setAttribute("data-theme", theme);
+  }, [theme]);
 
   return (
     <img
       className={" h-7 cursor-pointer " + className}
       onClick={() => {
-        setMode(oppose());
+        setTheme(oppose());
       }}
       src={"icons/" + oppose() + "mode.svg"}
     />
